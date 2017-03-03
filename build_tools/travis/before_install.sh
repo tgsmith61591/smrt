53c3bc82bdf4fdc18e5aaeb6019f9a74a24b4742
Taylor Smith <tgsmith61591@gmail.com>
2017-03-03 11:01:49 -0600

    Install specific GCC before-install

--- build_tools/travis/before_install.sh
+++ build_tools/travis/before_install.sh
@@ -5,4 +5,7 @@ set -e
 # before installing TensorFlow, we should install Bazel to compile TF
 sudo apt-get -qq update
 
+# we need GLIBCXX_3.4.19
+sudo apt-get install gcc-4.9
+
 # TODO: install bazel
